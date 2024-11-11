5ed2d784be724730207d07469968d80d001580a0
jay <sharmaji582009@gmail.com>
2024-11-11 20:49:39 +0530

    more things done

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 import { connectDB } from "./db/index.js";
+import { app } from "./app.js";
 
 connectDB()
-.then(
-
-)
+    .then(() => {
+        app.on("error", (error) => {
+            console.log("MONGO DB CONNECTION FAILED !!!", error);
+            throw error;
+        });
+        app.listen(process.env.PORT || 5173, () => {
+            console.log(`Server running on port ${process.env.PORT || 5173}`);
+            app.get("/", (req, res) => {
+                res.send("Welcome to the API");
+            });
+        });
+    })
+    .catch((error) => {
+        console.log("Server failed to start", error);
+    });
 
 dotenv.config({
-    path: "./env"
-},)
+    path: "./env",
+});
